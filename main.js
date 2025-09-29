f413a0a432a5179985eee72d6cdfc75c0b7b2be6
Sergio <Sergiopadilla0987@gmail.com>
2025-09-29 14:45:54 -0400

    finished tic tac toe game

--- main.js
+++ main.js
@@ -1,13 +1,91 @@
 
 const gameBoard = (function(){
     const board = Array(9).fill("");
+    function newPlayer(name, position) {
+        return{name, position}
+    }
+    const player1 = newPlayer("Sergio", "x");
+    console.log(player1.name);
+    const player2 = newPlayer("Cathy", "o");
+    console.log(player2.name);
+
+    let gameOver = false;
+    const winningCombos = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ];
+
+    function checkWinner(mark){
+        return winningCombos.some(combo => combo.every(i => board[i] === mark));
+    }
+
+    
+    const players = [player1, player2];
+    let currentPlayerIndex = 0;
+
+    function nextTurn() {
+        currentPlayerIndex = 1 -currentPlayerIndex;
+    }
+
     const container = document.querySelector(".container");
     board.forEach((item, index) => {
         const cell = document.createElement("div");
-        card.classList.add("cell");
+        cell.classList.add("cell");
+        cell.dataset.index = index;
         container.appendChild(cell);
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'lightblue';
+        });
+        cell.addEventListener('mouseleave', () => {
+            cell.style.backgroundColor = 'white';
+        });
+        cell.addEventListener('click', () => {
+            if(gameOver) return;
+            if (cell.textContent === ""){
+                cell.textContent = players[currentPlayerIndex].position;
+                const idx = Number(cell.dataset.index);
+                board[idx] = players[currentPlayerIndex].position;
+
+                const mark = players[currentPlayerIndex].position;
+                const playerName = players[currentPlayerIndex].name;
+                if(checkWinner(mark)) {
+                    console.log(`${playerName} wins!`);
+                    gameOver = true;
+                    return;
+                }
+                if (board.every(v => v !== "")){
+                    console.log("Draw!")
+                    gameOver = true;
+                    return;
+                }
+                nextTurn();
+            }
+            else {
+                alert('Try again');
+            }
+        })
+
     });
+    function reset() {
+        board.fill("");
+        const cells = container.querySelectorAll(".cell");
+        cells.forEach(cell => {
+            cell.textContent = "";
+        });
+        currentPlayerIndex = 0;
+        gameOver = false;
+    };
 
-    return {board};
+    const resetBtn = document.getElementById("resetBtn");
+    resetBtn.addEventListener("click", reset);
+    resetBtn.addEventListener("mouseenter", () =>{
+        resetBtn.style.backgroundColor = 'lightblue';
+    })
+    resetBtn.addEventListener("mouseleave", () =>{
+        resetBtn.style.backgroundColor = 'white';
+    })
+    
+    return {reset};
+
 })();
-
